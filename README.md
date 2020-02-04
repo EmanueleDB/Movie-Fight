8b43338aa4bfe0c3b77f6f5408d7c512c4409c9b
EmanueleDB <leleoioioi@gmail.com>
2020-02-04 17:23:03 +0100

    Fetching implemented and working perfectly

---
+++ README.md
@@ -0,0 +1,5 @@
+# Movies Fight
+
+## A funny App to compare movies based on their ratings and box office earnings!
+
+### Deployed version [here]()
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const fetchData = async () => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: '535e3db6',
-            s: 'avengers'
-        }
-    })
-    console.log(response.data)
-}
+const fetchData = async searchTerm => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "535e3db6",
+      s: searchTerm
+    }
+  });
+  console.log(response.data)
+};
 
-fetchData()
+const input = document.querySelector("input");
+
+
+const onInput = event => {
+  fetchData(event.target.value);
+};
+
+input.addEventListener("input", debounce(onInput, 500));
